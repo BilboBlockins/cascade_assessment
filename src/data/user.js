b8663d135ebf9bd78353d0bb6845b603b7751476
brett <abretthiebert@gmail.com>
2022-04-17 09:02:14 -0400

    add user data

---
+++ src/data/user.js
@@ -0,0 +1,11 @@
+export default {
+  "userId": 876543210,
+  "firstName": "Brett",
+  "lastName": "Hiebert",
+  "email": "abretthiebert@gmail.com",
+  "age": 34,
+  "accountType": "Checking",
+  "accountNumber": 123456789,
+  "statementBalance": "$400.00",
+  "currentBalance": "$400.00"
+}
--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,21 @@
 import { createStore } from 'vuex'
 import StatementData from '@/data/transactions.js'
+import UserData from '@/data/user.js'
 
 export default createStore({
   state: {
-    statement: StatementData
+    statement: StatementData,
+    user: UserData
+  },
+  mutations: {
+    UPDATE_STATEMENT(state, payload) {
+      console.log(state, payload)
+    },
+    UPDATE_USER(state, payload) {
+      console.log(state, payload)
+    }
+  },
+  actions: {
+    //...
   }
 })
