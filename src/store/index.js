6e7741858f3c9f16236f46a715df02a14298f3e0
brett <abretthiebert@gmail.com>
2022-04-17 11:20:06 -0400

    working pay instantly btn

--- src/store/index.js
+++ src/store/index.js
@@ -21,6 +21,24 @@ export default createStore({
       let newUserState = {...userState, ...payload}
       console.log("from user state action: ", newUserState)
       context.commit('UPDATE_USER', newUserState)
+    },
+    payPending(context, payload) {
+      let newStatement = context.state.statement
+      //filter the item from the not settled list
+      let currentPending = newStatement.NotSettled
+      let result = currentPending.filter(tx => tx.TransactionId !== payload.TransactionId)
+      newStatement.NotSettled = result
+      //update the tx array for each tx removed to show it billed
+      let newTxs = newStatement.Transactions.map(tx => {
+        if(tx.TransactionId === payload.TransactionId) {
+          return {...tx, Billed: true}
+        }
+        return tx
+      })
+      console.log('new txs from payp:', newTxs)
+      newStatement.Transactions = newTxs
+      console.log('in remove pending:', payload)
+      context.commit('UPDATE_STATEMENT', newStatement)
     }
   }
 })
